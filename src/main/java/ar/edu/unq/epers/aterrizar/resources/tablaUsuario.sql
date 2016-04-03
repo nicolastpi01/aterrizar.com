82c6c065aca341f891099a90b2c7522ff2659033
Damian Rafael Lattenero <damianlattenero@gmail.com>
2016-04-03 01:57:14 -0900

    script de creacion de tabla para usuarios mysql

---
+++ src/main/java/ar/edu/unq/epers/aterrizar/resources/tablaUsuario.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `usuario` (
+  `idusuario` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `nombreDeUsuario` varchar(45) DEFAULT NULL,
+  `nombreYApellido` varchar(45) DEFAULT NULL,
+  `usuariocol2` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `contrasenia` varchar(45) DEFAULT NULL,
+  `codigoDeEmail` varchar(45) DEFAULT NULL,
+  `estaRegistradoEmail` tinyint(1) DEFAULT '0',
+  `nacimiento` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idusuario`),
+  UNIQUE KEY `nombreDeUsuario_UNIQUE` (`nombreDeUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8
